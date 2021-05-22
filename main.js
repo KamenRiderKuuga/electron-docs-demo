72ca0313de3709daa1874a3fc2536fbbf063dc8a
GadHao <39508075+GadHao@users.noreply.github.com>
2021-05-22 23:42:36 +0800

    feat: notification on start

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
